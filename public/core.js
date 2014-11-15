d620de1582613ce493b70da2497226532d8b47b0
viancha <viancha@paypal.com>
2014-11-14 21:22:14 -0800

    angular changes

--- public/core.js
+++ public/core.js
@@ -1,8 +1,10 @@
 // public/core.js
-var scotchTodo = angular.module('scotchTodo', []);
+var transactions = angular.module('transactions', []);
 
 function mainController($scope, $http) {
-    $scope.formData = {};
+    
+
+    $scope.melody = {"records":[{"transaction":{"apicalls":[{"correlation_id":"sdft34aef3q4t","downstream_calls":[{"service":"mayfly"},{"service":"userread"},{"service":"merchant preferences"}],"end_time":"435344352345","endpoint":"post.v1.carts.cart","start_time":"2453464563463"},{"correlation_id":"sdft34aef3q4t","downstream_calls":[{"service":"mayfly"},{"service":"fundingpolicyservice"}],"end_time":"435344352345","endpoint":"get.v1.carts.eligiblepaymentmethods","start_time":"2453464563463"},{"correlation_id":"sdft34aef3q4t","downstream_calls":[{"service":"mayfly"},{"service":"moneyplanningservice"}],"end_time":"435344352345","endpoint":"post.v1.carts.gfo","start_time":"2453464563463"},{"correlation_id":"sdft34aef3q4t","downstream_calls":[{"service":"mayfly"},{"service":"fulfillmentinternalserv"},{"service":"PLS"}],"end_time":"435344352345","endpoint":"post.v1.carts.pay","start_time":"2453464563463"}],"token":"EC-1233542GR1232354"}}]};;
 
     // when landing on the page, get all todos and show them
     $http.get('/api/todos')
